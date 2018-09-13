5f3386acb81fcf5b7a1e4f1cd448b5f24325add0
Michał Kołodziejczyk <MKO@GLOBAL.PL>
2018-09-13 08:56:00 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Xsl;
+
+namespace XmlPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
